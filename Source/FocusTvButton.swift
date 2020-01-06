2631061705fed61c0de1b6562d9afe1aa7dd8f3f
AVVS <v@makeomatic.ca>
2020-01-06 13:06:35 -0800

    feat: auto deploy

--- Source/FocusTvButton.swift
+++ Source/FocusTvButton.swift
@@ -96,41 +96,41 @@ open class FocusTvButton: UIButton {
     @IBInspectable public var gradientEndPoint: CGPoint = CGPoint(x: 1, y: 1) {
         didSet { updateView() }
     }
-    
+
     open override var isSelected: Bool {
         didSet { updateView() }
     }
-    
+
     open override var buttonType: UIButton.ButtonType {
         return .custom
     }
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         updateView()
     }
-    
+
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         updateView()
     }
-    
+
     override open func awakeFromNib() {
         super.awakeFromNib()
         updateView()
     }
-    
+
     override open func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
         coordinator.addCoordinatedAnimations({
             self.isFocused ? self.applyFocusedStyle() : self.applyUnfocusedStyle()
         }, completion: nil)
     }
-    
+
     override open func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
         guard presses.first?.type == .select else {
             return super.pressesBegan(presses, with: event)
         }
-        
+
         UIView.animate(
             withDuration: animationDuration,
             animations: {
@@ -140,7 +140,7 @@ open class FocusTvButton: UIButton {
                 self.layer.shadowOffset = CGSize(width: 0, height: 10)
         })
     }
-    
+
     override open func pressesCancelled(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
         guard presses.first?.type == .select else {
             return super.pressesCancelled(presses, with: event)
@@ -155,7 +155,7 @@ open class FocusTvButton: UIButton {
                 self.layer.shadowOffset = self.shadowOffSetFocused
         })
     }
-    
+
     override open func pressesEnded(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
         guard presses.first?.type == .select else {
             return super.pressesEnded(presses, with: event)
@@ -170,26 +170,26 @@ open class FocusTvButton: UIButton {
                 self.layer.shadowOffset = self.shadowOffSetFocused
         })
     }
-    
+
     // MARK: - Private
-    
+
     private var selectedGradientBackgroundColors: [CGColor] {
         let endColor = selectedBackgroundEndColor ?? selectedBackgroundColor
         return [selectedBackgroundColor.cgColor, endColor.cgColor]
     }
-    
+
     private var focusedGradientBackgroundColors: [CGColor] {
         let endColor = focusedBackgroundEndColor ?? focusedBackgroundColor
         return [focusedBackgroundColor.cgColor, endColor.cgColor]
     }
-    
+
     private var normalGradientBackgroundColors: [CGColor] {
         let endColor = normalBackgroundEndColor ?? normalBackgroundColor
         return [normalBackgroundColor.cgColor, endColor.cgColor]
     }
-    
+
     private let gradientView = GradientView()
-    
+
     private func updateView() {
         setUpGradientView()
         layer.cornerRadius = cornerRadius
@@ -200,7 +200,7 @@ open class FocusTvButton: UIButton {
         layer.shadowRadius = focusedShadowRadius
         layer.shadowColor = shadowColor
         layer.shadowOffset = shadowOffSetFocused
-        
+
         if isFocused {
             transform = CGAffineTransform(scaleX: focusedScaleFactor, y: focusedScaleFactor)
         }
@@ -208,14 +208,14 @@ open class FocusTvButton: UIButton {
             transform = CGAffineTransform.identity
         }
     }
-    
+
     private func setUpGradientView() {
         gradientView.frame = bounds
         gradientView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         gradientView.layer.cornerRadius = cornerRadius
         gradientView.startPoint = gradientStartPoint
         gradientView.endPoint = gradientEndPoint
-        
+
         if isFocused {
             gradientView.colors = focusedGradientBackgroundColors
         }
@@ -225,7 +225,7 @@ open class FocusTvButton: UIButton {
         else {
             gradientView.colors = normalGradientBackgroundColors
         }
-        
+
         if let imageView = imageView {
             insertSubview(gradientView, belowSubview: imageView)
         }
@@ -236,7 +236,7 @@ open class FocusTvButton: UIButton {
             addSubview(gradientView)
         }
     }
-    
+
     private func applyFocusedStyle() {
         UIView.animate(
             withDuration: animationDuration,
@@ -246,7 +246,7 @@ open class FocusTvButton: UIButton {
             },
             completion: nil)
     }
-    
+
     private func applyUnfocusedStyle() {
         UIView.animate(
             withDuration: animationDuration,
@@ -259,43 +259,43 @@ open class FocusTvButton: UIButton {
 }
 
 final private class GradientView: UIView {
-    
+
     override class var layerClass: AnyClass {
         return CAGradientLayer.self
     }
-    
+
     var colors: [Any]? {
         set {
             gradientLayer.colors = newValue
         }
-        
+
         get {
             return gradientLayer.colors
         }
     }
-    
+
     var startPoint: CGPoint {
         set {
             gradientLayer.startPoint = newValue
         }
-        
+
         get {
             return gradientLayer.startPoint
         }
     }
-    
+
     var endPoint: CGPoint {
         set {
             gradientLayer.endPoint = newValue
         }
-        
+
         get {
             return gradientLayer.endPoint
         }
     }
-    
+
     // MARK: - Private
-    
+
     private lazy var gradientLayer: CAGradientLayer = {
         return layer as! CAGradientLayer
     }()
